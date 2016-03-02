697abd36ee29723ee312c17dc09c78a971bded17
Ben Huggett <benhuggett@btinternet.com>
2016-03-02 17:53:33 +0000

    Modify cMotorDriver to sleep PWM outputs when the motor isn't moving.

--- Trunk/cMotorDriver.cpp
+++ Trunk/cMotorDriver.cpp
@@ -22,27 +22,54 @@ cMotorDriver::cMotorDriver(cPwmBoard* pPwmBoardInstance)
 // Choose a motor to set to a speed between -50ish and 50ish (??)
 void cMotorDriver::setMotorSpeed(MOTORS_T motor, int speed)
 {
-	int duty = speed + 50;
-	if(duty < 2) 		duty = 2;
-	else if(duty > 98) 	duty = 98;
+	int duty;
 
+	// Scale input to give more meaningful output.
+	if      (speed <-5)	duty = (speed+50) *7/9;
+	else if (speed > 5)	duty = (speed-5) *7/9 + 65;
+	else 				duty = 50;
+
+	// Avoid maximing out duty cycle.
+	if     (duty < 2) 	duty = 2;
+	else if(duty >98) 	duty = 98;
+		
 	switch(motor)
 	{
 	case MOTOR_A: 
 		pPwmBoard->setPwm(PWM_0,duty);
 		pPwmBoard->setPwmInv(PWM_1,duty);
+		if(duty == 50)		
+		{
+			pPwmBoard->kill(PWM_0);
+			pPwmBoard->kill(PWM_1);
+		}		
 		break;
 	case MOTOR_B:
 		pPwmBoard->setPwm(PWM_2,duty);
 		pPwmBoard->setPwmInv(PWM_3,duty);
+		if(duty == 50)		
+		{
+			pPwmBoard->kill(PWM_2);
+			pPwmBoard->kill(PWM_3);
+		}		
 		break;		
 	case MOTOR_C:
 		pPwmBoard->setPwm(PWM_4,duty);
 		pPwmBoard->setPwmInv(PWM_5,duty);
+		if(duty == 50)		
+		{
+			pPwmBoard->kill(PWM_4);
+			pPwmBoard->kill(PWM_5);
+		}		
 		break;
 	case MOTOR_D:
 		pPwmBoard->setPwm(PWM_6,duty);
 		pPwmBoard->setPwmInv(PWM_7,duty);
+		if(duty == 50)		
+		{
+			pPwmBoard->kill(PWM_6);
+			pPwmBoard->kill(PWM_7);
+		}		
 		break;
 	default:
 		std::cerr << "Invalid motor being set!" << std::endl;
