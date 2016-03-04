b6185f4222bfc767315fae2929f9acbd26eb6e56
gpeh1g14 <gpeh1g14>
2016-03-04 09:57:01 +0000

    Pi changes 4/3/16

--- Trunk/main.cpp
+++ Trunk/main.cpp
@@ -43,12 +43,14 @@ int main()
 
     while(1)
     {
-	delay(200);
+	delay(30);
         //if(queryCVMode())   cvMode();     //TODO: Work out how to integrate this with the control loop properly.
 
         vel = my_RC.getInputMovementSpeed();
         rot = my_RC.getInputRotationSpeed();
-
+	//vel.first = 0;
+	//vel.second = 500;
+	rot = 0;
         my_motors.setMotorSpeed(MOTOR_A, (rot/ROTATION_DIVISOR + vel.first)/MOTOR_DRIVER_DIVISOR);
         my_motors.setMotorSpeed(MOTOR_B, (rot/ROTATION_DIVISOR - vel.first)/MOTOR_DRIVER_DIVISOR);
         my_motors.setMotorSpeed(MOTOR_C, (rot/ROTATION_DIVISOR + vel.second)/MOTOR_DRIVER_DIVISOR);
